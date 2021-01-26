472d4e840c14f9254f0fe89ab7adca6e144ba008
FBOL-SS <67643371+FBOL-SS@users.noreply.github.com>
2021-01-26 10:15:48 -0600

    Add files via upload

--- pdf_viewer/__manifest__.py
+++ pdf_viewer/__manifest__.py
@@ -3,9 +3,9 @@
 {
     'name': 'PDF Viewer',
     'summary': 'PDF Viewer Widget.',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Nebil Aydi',
-    'website': 'https://github.com/aydi-nebil/web/tree/11.0/pdf_viewer',
+    'website': 'https://github.com/aydi-nebil/web/tree/12.0/pdf_viewer',
     'license': 'AGPL-3',
     'application': False,
     'installable': True,
